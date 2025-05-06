2446774f51435702681b817c85cfffad3c6bbd66
Juan <marcoreus24102001@gmail.com>
2025-05-06 17:59:01 -0500

    Agregué por defecto el listado de 3 productos en un UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import MyOrders from "./MyOrders";
-import AddEditOrder from "./AddEditOrder";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyOrders from './MyOrders';
+import AddEditOrder from './AddEditOrder';
 
 function App() {
   const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (orders.length === 0) {
+      setOrders([
+        {
+          id: 1564543,
+          name: 'Chocolate',
+          quantity: 21,
+          price: 8.5,
+          status: 'Pendiente'
+        },
+        {
+          id: 25425723,
+          name: 'Leche Gloria',
+          quantity: 65,
+          price: 5.5,
+          status: 'Pendiente'
+        },
+        {
+          id: 3573753,
+          name: 'Avena 3 Ositos', 
+          quantity: 39,
+          price: 2.5,
+          status: 'Pendiente'
+        }
+      ]);
+    }
+  }, [orders]);
   
+
   return (
     <Router>
       <Routes>
